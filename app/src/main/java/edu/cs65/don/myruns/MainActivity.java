6f92a7511aafdec3a88474a0c4e418c1ba22b707
user-don <don@staynermail.com>
2016-04-02 11:17:22 -0400

    4/1 in-class notes
    Adding Dialog for photos

--- app/src/main/java/edu/cs65/don/myruns/MainActivity.java
+++ app/src/main/java/edu/cs65/don/myruns/MainActivity.java
@@ -1,13 +1,116 @@
 package edu.cs65.don.myruns;
 
+import android.app.DialogFragment;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXAMPLE_KEY = "exampleKey";
+    private static final ID_PHOTO_PICKER_FROM_CAMERA = 0;
+    private ImageView mImageView;
+    private Uri mImageCaptureUri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView) findViewById(R.id.prof_photo);
+
+        if (savedInstanceState != null) {
+            mImageCaptureUri = savedInstanceState.getParcelable(EXAMPLE_KEY);
+        }
+
+
     }
+
+    @Override
+    public void onRestoreInstanceState (Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // Restore UI state from the savedInstanceState.
+        // This bundle has also been passed to onCreate.
+        // Will only be called if the Activity has been
+        // killed by the system since it was last visible.
+    }
+
+    /**
+     * Called before subsequent visible lifetimes for an activity process
+     */
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        // Load changes knowing that the Activity has already
+        // been visible within this process.
+    }
+
+    // Called at the start of the visible lifetime.
+    @Override
+    public void onStart(){
+        super.onStart();
+        // Apply any required UI change now that the Activity is visible.
+    }
+
+    // Called at the start of the active lifetime.
+    @Override
+    public void onResume(){
+        super.onResume();
+        // Resume any paused UI updates, threads, or processes required
+        // by the Activity but suspended when it was inactive.
+    }
+
+    // Called to save UI state changes at the
+    // end of the active lifecycle.
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save UI state changes to the savedInstanceState.
+        // This bundle will be passed to onCreate and
+        // onRestoreInstanceState if the process is
+        // killed and restarted by the run time.
+        super.onSaveInstanceState(savedInstanceState);
+        Log.d("app", "onSaveInstanceState() called");
+
+        // call this to save something.
+        //savedInstanceState.putParcelable(URI_INSTANCE_STATE_KEY, mImageCaptureUri);
+    }
+
+    // Called at the end of the active lifetime.
+    @Override
+    public void onPause(){
+        // Suspend UI updates, threads, or CPU intensive processes
+        // that don't need to be updated when the Activity isn't
+        // the active foreground Activity.
+        super.onPause();
+    }
+
+    // Called at the end of the visible lifetime.
+    @Override
+    public void onStop(){
+        // Suspend remaining UI updates, threads, or processing
+        // that aren't required when the Activity isn't visible.
+        // Persist all edits or state changes
+        // as after this call the process is likely to be killed.
+        super.onStop();
+    }
+
+    // Sometimes called at the end of the full lifetime.
+    @Override
+    public void onDestroy(){
+        // Clean up any resources including ending threads,
+        // closing database connections etc.
+        super.onDestroy();
+    }
+
+    // display dialog fragment
+    public void displayDialog(int id) {
+        DialogFragment fragment = SelectProfileImageDialogFragment.newInstance(id);
+        fragment.show(getFragmentManager(), "dialog_fragment_photo_picker");
+    }
+
+
+
 }
---
+++ app/src/main/java/edu/cs65/don/myruns/SelectProfileImageDialogFragment.java
@@ -0,0 +1,71 @@
+package edu.cs65.don.myruns;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+/**
+ * Created by don on 3/31/16.
+ */
+public class SelectProfileImageDialogFragment extends DialogFragment {
+
+    private static final String DIALOG_ID_KEY = "id_key";
+    private static final int DIALOG_ID_PHOTO_PICKER = 1;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // get ID to figure out what dialog we want to show
+        int dialog_id = getArguments().getInt(DIALOG_ID_KEY);
+
+        final Activity parent = getActivity();
+
+        switch(dialog_id) {
+            case DIALOG_ID_PHOTO_PICKER:
+                // put stuff in here
+
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.select_profile_image);
+
+        builder.setItems(R.array.ui_profile_photo_picker_items, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // the which argument contains the index position of the selected item
+                switch(which) {
+                    case 0:
+                        // take picture from camera
+                    case 1:
+                        // do something else
+
+                }
+            }
+        });
+        return builder.create();
+
+
+        // all of our options are stored in an array in rsources
+
+//        DialogInterface.OnClickListener dlistener = new DialogInterface.OnClickListener() {
+//            public void onClick(DialogInterface dialog, int item) {
+//
+//            }
+//        };
+                //builder.setItems()
+
+
+    }
+
+    public static SelectProfileImageDialogFragment newInstance(int dialog_id) {
+        SelectProfileImageDialogFragment frag = new SelectProfileImageDialogFragment();
+        Bundle args = new Bundle();
+        // we want to pass ID to the activity
+        args.putInt(DIALOG_ID_KEY, dialog_id);
+        frag.setArguments(args);
+    }
+
+
+
+}
