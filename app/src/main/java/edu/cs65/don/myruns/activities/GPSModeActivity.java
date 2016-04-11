52a89dc7879b144c19778e35a0be242803af67c8
user-don <don@staynermail.com>
2016-04-11 14:55:38 -0400

    Added GPS mode activity

---
+++ app/src/main/java/edu/cs65/don/myruns/activities/GPSModeActivity.java
@@ -0,0 +1,13 @@
+package edu.cs65.don.myruns.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GPSModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gpsmode);
+    }
+}
