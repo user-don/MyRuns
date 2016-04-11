eae052b26c9a70b8ed24c1e886dc7a11ff830637
user-don <don@staynermail.com>
2016-04-11 19:58:16 -0400

    Able to pull up account preferences

--- app/src/main/java/edu/cs65/don/myruns/fragments/SettingsFragment.java
+++ app/src/main/java/edu/cs65/don/myruns/fragments/SettingsFragment.java
@@ -1,25 +1,59 @@
 package edu.cs65.don.myruns.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import edu.cs65.don.myruns.R;
+import edu.cs65.don.myruns.activities.AccountPreferencesActivity;
+import edu.cs65.don.myruns.activities.MainActivity;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragment {
+
+    public static final int ACCOUNT_PREFERENCES = 0;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // load preferences from XML
+        addPreferencesFromResource(R.xml.preferences);
+
+        // accessing preferences
+        PreferenceScreen ps = (PreferenceScreen) getPreferenceScreen();
+        // add listener for account_preferences
+        Preference account_preferences = (Preference) ps.findPreference("account_preferences");
+        Preference.OnPreferenceClickListener account_preferences_listener =
+                getOnPreferenceClickListener(ACCOUNT_PREFERENCES);
+        account_preferences.setOnPreferenceClickListener(account_preferences_listener);
+
+
+    }
+
+    private Preference.OnPreferenceClickListener getOnPreferenceClickListener(int preference_id) {
+        if (preference_id == ACCOUNT_PREFERENCES) {
+            return new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    // open intent
+                    Intent intent = new Intent(getActivity(), AccountPreferencesActivity.class);
+                    startActivity(intent);
+                    return true;
+                }
+            };
+        }
+        return null;
     }
 
 }
