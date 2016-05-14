9687cb05519f49b40e3b960a72e652a694b10b76
Matt McFarland <mcfarland.mattr@gmail.com>
2016-05-14 09:54:34 -0400

    added datastore functions

--- backend/src/main/java/com/example/don/myapplication/backend/data/EEDataStore.java
+++ backend/src/main/java/com/example/don/myapplication/backend/data/EEDataStore.java
@@ -1,5 +1,7 @@
 package com.example.don.myapplication.backend.data;
 
+import java.util.ArrayList;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.google.appengine.api.datastore.DatastoreService;
@@ -13,6 +15,7 @@ import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.appengine.api.datastore.Transaction;
+import com.sun.corba.se.spi.activation.Server;
 
 /**
  * Created by McFarland on 5/13/16.
@@ -25,4 +28,100 @@ public class EEDataStore {
     // make static so it's always available
     private static final DatastoreService mDatastore = DatastoreServiceFactory
             .getDatastoreService();
+
+    private static Key getKey() {
+        return KeyFactory.createKey(ServerEE.EE_PARENT_ENTITY_NAME,
+                ServerEE.EE_PARENT_KEY_NAME);
+    }
+
+    public static ServerEE getEEByID(Long ID, Transaction txn) {
+        Entity result = null;
+        try {
+            result = mDatastore.get(KeyFactory.createKey(getKey(),
+                    ServerEE.EE_ENTITY_NAME, ID));
+        } catch (Exception ex) {
+
+        }
+
+        return getEEFromEntity(result);
+    }
+
+    public static ArrayList<ServerEE> getAllEntries() {
+        ArrayList<ServerEE> entries = new ArrayList<ServerEE>();
+
+        // create query with no filter to get all entries
+        Query query = new Query(ServerEE.EE_ENTITY_NAME);
+        query.setFilter(null);
+        query.setAncestor(getKey());
+
+        // add all entries to results list
+        for (Entity entity : mDatastore.prepare(query).asIterable()) {
+            ServerEE entry = getEEFromEntity(entity);
+            if (entity != null) {
+                entries.add(entry);
+            }
+        }
+
+        return entries;
+    }
+
+    // returns true if entry was deleted, false otherwise
+    public static boolean deleteEntry(String strID) {
+
+        try {
+            long ID = Long.parseLong(strID);
+
+            Filter filter = new FilterPredicate(ServerEE.FIELD_NAME_ID,
+                    FilterOperator.EQUAL, ID);
+
+            Query query = new Query(ServerEE.EE_ENTITY_NAME);
+            query.setFilter(filter);
+
+            Entity result = mDatastore.prepare(query).asSingleEntity();
+            if (result != null) {
+                mDatastore.delete(result.getKey());
+                return true;
+            }
+
+        } catch (NumberFormatException e) {
+            mLogger.log(Level.INFO, "unable to parse string ID: " + strID);
+        }
+
+        return false;
+    }
+
+    public static void deleteAllEntries() {
+
+        // create query with no filter to get all entries
+        Query query = new Query(ServerEE.EE_ENTITY_NAME);
+        query.setFilter(null);
+        query.setAncestor(getKey());
+
+        // delete all entries
+        for (Entity entity : mDatastore.prepare(query).asIterable()) {
+            if (entity != null) {
+                mDatastore.delete(entity.getKey());
+            }
+        }
+
+    }
+
+    private static ServerEE getEEFromEntity(Entity entity) {
+        if (entity == null) {
+            return null;
+        }
+
+        return new ServerEE(
+                (Long) entity.getProperty(ServerEE.FIELD_NAME_ID),
+                (String) entity.getProperty(ServerEE.FIELD_NAME_INPUT_TYPE),
+                (String) entity.getProperty(ServerEE.FIELD_NAME_ACTIVITY_TYPE),
+                (String) entity.getProperty(ServerEE.FIELD_NAME_DATE_TIME),
+                (String) entity.getProperty(ServerEE.FIELD_NAME_DURATION),
+                (String) entity.getProperty(ServerEE.FIELD_NAME_DISTANCE),
+                (String) entity.getProperty(ServerEE.FIELD_NAME_AVG_SPEED),
+                (String) entity.getProperty(ServerEE.FIELD_NAME_CALORIES),
+                (String) entity.getProperty(ServerEE.FIELD_NAME_CLIMB),
+                (String) entity.getProperty(ServerEE.FIELD_NAME_HEART_RATE),
+                (String) entity.getProperty(ServerEE.FIELD_NAME_COMMENT));
+    }
 }
